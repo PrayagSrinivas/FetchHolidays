fef90d3c4727e5081e3b2a51e4de936bb9ce2d53
sidhartha das <jovialwithigit@gmail.com>
2020-11-10 09:33:08 +0530

    Initial Commit

---
+++ HolidayDetails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolidayDetails
+//
+//  Created by Jovial Software on 10/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
